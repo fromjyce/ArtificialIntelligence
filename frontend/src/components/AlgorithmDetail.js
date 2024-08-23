40f7e45aa5623a77f0b9f8a4023c13da4b1865e9
Jayashre <jaya2004kra@gmail.com>
2024-08-23 12:12:23 +0530

    added components of blind algo page

---
+++ frontend/src/components/AlgorithmDetail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AlgorithmDetail.css';
+
+const AlgorithmDetail = ({ title, description }) => (
+    <div className="algorithm-detail">
+        <div className="detail-text">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+        <div className="detail-button-container">
+            <button className="detail-button">Learn More</button>
+        </div>
+    </div>
+);
+
+export default AlgorithmDetail;
--- frontend/src/components/BlindAlgoList.js
+++ frontend/src/components/BlindAlgoList.js
@@ -1,13 +1,26 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AlgorithmDetail from './AlgorithmDetail';
+import './BlindAlgoList.css';
 
 const BlindAlgoList = () => {
     return (
-        <div className="blind-algorithm-list">
-            <h1>Blind Algorithm Details</h1>
-            <p>Details about the selected algorithm will be displayed here.</p>
-            <Link to="/">Back to Home</Link>
+        <div className="algorithm-details">
+            <div className="details-container">
+                <AlgorithmDetail 
+                    title="British Museum Search" 
+                    description="Description of British Museum Search algorithm goes here." 
+                />
+                <AlgorithmDetail 
+                    title="Another Algorithm" 
+                    description="Description of another algorithm goes here." 
+                />
+                <AlgorithmDetail 
+                    title="Yet Another Algorithm" 
+                    description="Description of yet another algorithm goes here." 
+                />
+            </div>
+            <Link to="/" className="back-button">Back to Home</Link>
         </div>
     );
 };
