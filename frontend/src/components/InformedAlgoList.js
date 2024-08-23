9236b5e7c1949711b844fed505d6607f693dae8c
Jayashre <jaya2004kra@gmail.com>
2024-08-23 21:34:31 +0530

    added informed list

--- frontend/src/components/InformedAlgoList.js
+++ frontend/src/components/InformedAlgoList.js
@@ -1,31 +1,35 @@
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import AlgorithmDetail from './AlgorithmDetail';
 import '../styles/InformedAlgoList.css';
 
 const InformedAlgoList = () => {
+    const navigate = useNavigate();
     return (
         <div className="algorithm-details">
             <div className="details-container">
-                <AlgorithmDetail 
-                    title="British Museum Search" 
-                    description="Description of British Museum Search algorithm goes here." 
-                />
-                <AlgorithmDetail 
-                    title="Another Algorithm" 
-                    description="Description of another algorithm goes here." 
-                />
-                <AlgorithmDetail 
-                    title="Yet Another Algorithm" 
-                    description="Description of yet another algorithm goes here." 
-                />
-                <AlgorithmDetail 
-                    title="Yet Another Algorithm" 
-                    description="Description of yet another algorithm goes here." 
-                />
-            </div>
-            <Link to="/" className="back-button">Back to Home</Link>
+                    <AlgorithmDetail 
+                        title="Hill Climbing Method" 
+                        description="Hill climbing uses a heuristic to move towards the goal by selecting the most promising path based on the heuristic evaluation." 
+                        link="/hill-climbing-method" 
+                    />
+                    <AlgorithmDetail 
+                        title="Beam Search Method" 
+                        description="Beam search is a variation of BFS that only keeps a fixed number of best nodes at each level, based on heuristic measures." 
+                        link="/beam-search-method" 
+                    />
+                    <AlgorithmDetail 
+                        title="Oracle Search Method" 
+                        description="Oracle Search Method refers to a theoretical or idealized approach where an oracle provides perfect knowledge or guidance during the search process." 
+                        link="/oracle-search-method" 
+                    />
+                    <AlgorithmDetail 
+                        title="Best First Search" 
+                        description="Best First Search explores a search space by prioritizing nodes that appear to be the most promising" 
+                        link="best-first-search" 
+                    />
+                    </div>
+            <button className="back-button" onClick={() => navigate('/')}>Back</button>
         </div>
     );
 };
