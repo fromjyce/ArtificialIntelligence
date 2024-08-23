b3060f58810a05fc890260d84e96c445f30210ee
Jayashre <jaya2004kra@gmail.com>
2024-08-23 18:47:51 +0530

    added link parameter

--- frontend/src/components/AlgorithmDetail.js
+++ frontend/src/components/AlgorithmDetail.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/AlgorithmDetail.css';
 
-const AlgorithmDetail = ({ title, description }) => (
+const AlgorithmDetail = ({ title, description, link }) => (
     <div className="algorithm-detail">
         <div className="detail-text">
             <h2>{title}</h2>
             <p>{description}</p>
         </div>
         <div className="detail-button-container">
-            <button className="detail-button">Learn More</button>
+            <Link to={link}>
+                <button className="detail-button">Learn More</button>
+            </Link>
         </div>
     </div>
 );
